c2d49b38baa8d72ddf4254c28ba6b63ea3cb79ef
Priyanshu <shardapriyanshu10@gmail.com>
2026-05-20 15:49:36 +0530

    Updated backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://studynotion-lms-xiqz.onrender.com',
+  baseURL: "https://studynotion-lms-18lfd.onrender.com",
 });
 
 // 🔥 interceptor
